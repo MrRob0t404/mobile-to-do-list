920e8eeff187b949a133a9bcebf336334903c434
Simon Gaviria <simongaviria1@gmail.com>
2024-09-27 12:29:33 -0400

    add types to react-vector-icons for eslint fix

---
+++ @types/react-native-vector-icons.d.ts
@@ -0,0 +1,5 @@
+declare module "react-native-vector-icons/Feather" {
+  // Update the import path for IconProps
+  import { IconProps } from "react-native-vector-icons"; // Adjusted path
+  export default function FeatherIcon(props: IconProps): JSX.Element;
+}
